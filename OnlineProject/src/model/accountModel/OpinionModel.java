648bf2a81c52d8f56ec9ea90d3d3069e2d47bce7
Mahdi <mahdijm.bb@gmail.com>
2023-03-31 01:38:27 +0430

    creat opinion class

---
+++ OnlineProject/src/model/accountModel/OpinionModel.java
@@ -0,0 +1,10 @@
+package model.accountModel;
+
+public class OpinionModel {
+    CustomerModel user ;
+    String productId;
+    String opinion;
+    int opinionStatus;// this is that the opinion is accepted or rejected or admin do not answer yet!
+    boolean IsUserBuyProduct;
+
+}
