bf04523c4bcddf0988ceb4b6cadeb359f8b30a44
Mahdi <mahdijm.bb@gmail.com>
2023-03-31 03:46:31 +0430

    add full name to fields

--- OnlineProject/src/model/accountModel/CustomerModel.java
+++ OnlineProject/src/model/accountModel/CustomerModel.java
@@ -6,14 +6,16 @@ import model.productModel.ProductsModel;
 import java.util.ArrayList;
 
 public class CustomerModel extends AccountModel{
+    private String fullName;
     final private ArrayList<ProductsModel> cart ;
     final private ArrayList<ProductsModel> purchaseHistory;
     private double balance ;
-    CustomerModel(String name,String email ,String phoneNumber , String password,double balance){
+    CustomerModel(String name,String email ,String phoneNumber , String password,double balance,String fullName){
         super(name,email,phoneNumber,password);
         this.balance=balance;
         cart = new ArrayList<>();
         purchaseHistory=new ArrayList<>();
+        this.fullName=fullName;
     }
 
     public ArrayList<ProductsModel> getCart() {
@@ -30,5 +32,13 @@ public class CustomerModel extends AccountModel{
 
     public void setBalance(double balance) {
         this.balance = balance;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 }
