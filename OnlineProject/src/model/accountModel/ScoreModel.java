3fe14cd09a03cbce19c8d6bbfc23071bb901371a
Mahdi <mahdijm.bb@gmail.com>
2023-03-31 01:38:00 +0430

    creat Score class

---
+++ OnlineProject/src/model/accountModel/ScoreModel.java
@@ -0,0 +1,11 @@
+package model.accountModel;
+
+import model.productModel.ProductsModel;
+
+public class ScoreModel {
+    private CustomerModel buyer ;
+    private double score;
+    private ProductsModel purchasedProduct;
+
+
+}
