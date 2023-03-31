ec383c795643e92ededc84f655f1ece1c6fc3b87
Mahdi <mahdijm.bb@gmail.com>
2023-04-01 01:05:32 +0430

    just for test

---
+++ OnlineProject/src/controller/AdminController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import model.accountModel.AdminModel;
+
+public class AdminController {
+    AdminModel admin = new AdminModel("admin","admin@gmail.com","09131234567","admin");
+
+
+
+}
