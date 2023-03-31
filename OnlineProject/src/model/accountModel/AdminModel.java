45b0467a0c544e3585625511ac0cfe04b5ccc760
Mahdi <mahdijm.bb@gmail.com>
2023-04-01 02:23:26 +0430

    add list of comments and creat getter for lists

--- OnlineProject/src/model/accountModel/AdminModel.java
+++ OnlineProject/src/model/accountModel/AdminModel.java
@@ -7,11 +7,15 @@ import java.util.ArrayList;
 public class AdminModel extends AccountModel{
 
     private ArrayList<ProductsModel> productsOfStore;
+    private ArrayList<String> comments;
+
+
 
     private static AdminModel admin ;
     public AdminModel(String name,String email ,String phoneNumber , String password){
         super(name,email,phoneNumber,password);
         productsOfStore = new ArrayList<>();
+        comments =new ArrayList<>();
     }
 
     public static AdminModel getAdmin(String name,String email ,String phoneNumber , String password) {
@@ -23,5 +27,13 @@ public class AdminModel extends AccountModel{
 
     public static AdminModel getAdmin() {
         return admin;
+    }
+
+    public ArrayList<ProductsModel> getProductsOfStore() {
+        return productsOfStore;
+    }
+
+    public ArrayList<String> getComments() {
+        return comments;
     }
 }
