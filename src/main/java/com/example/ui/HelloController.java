269fea428f6401bb62deeda8cf9826e5cdd16587
Mahdi <mahdijm.bb@gmail.com>
2023-05-26 18:21:08 +0330

    main page is completed!

--- src/main/java/com/example/ui/HelloController.java
+++ src/main/java/com/example/ui/HelloController.java
@@ -1,14 +1,70 @@
 package com.example.ui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button AdminButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button LoginButton;
+
+    @FXML
+    private Button SignUpButton;
+
+    @FXML
+    private ImageView buyPic;
+
+    @FXML
+    private Button productButton;
+
+    @FXML
+    void AdminButtonAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("admin-login-controller.fxml")));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Sign up page");
+        stage.show();
     }
+
+    @FXML
+    void LoginButtonAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login-user-controller.fxml")));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) (event.getSource())).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Sign up page");
+        stage.show();
+    }
+
+    @FXML
+    void SignUpButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void productButtonAction(ActionEvent event) {
+
+    }
+
+
+
 }
